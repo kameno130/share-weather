4bdefb5775b3f1d623b8ae29bd5620471b59af64
kameno130 <51069500+kameno130@users.noreply.github.com>
2019-07-23 17:20:05 +0900

    login制限の追加

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,5 +1,7 @@
 class SharesController < ApplicationController
 
+  before_action :move_to_login,  except: [:index]
+
   def index
   end
 
@@ -7,5 +9,10 @@ class SharesController < ApplicationController
   end
 
   def create
+  end
+
+  private
+  def move_to_login
+    redirect_to new_user_registration_path unless user_signed_in?
   end
 end
