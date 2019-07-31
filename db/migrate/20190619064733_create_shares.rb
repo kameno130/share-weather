06039760714f7c2338d4074482172d30a574e727
kameno130 <51069500+kameno130@users.noreply.github.com>
2019-07-31 13:36:53 +0900

    投稿昨日のカラム変更

--- db/migrate/20190619064733_create_shares.rb
+++ db/migrate/20190619064733_create_shares.rb
@@ -1,9 +1,9 @@
 class CreateShares < ActiveRecord::Migration[5.0]
   def change
     create_table :shares do |t|
-      t.text :content
-      t.text   :image
-      t.text   :location
+      t.string :content
+      t.string   :location
+      t.string :title
       t.integer :user_id
       t.timestamps
     end
